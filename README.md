9bce5bb8f1bae288c0f58b0e1c14111fb4007c96
cryi <cryi@tutanota.com>
2021-03-08 17:38:15 +0100

    readme and setup script

---
+++ README.md
@@ -0,0 +1,52 @@
+# Setup
+
+`wget https://raw.githubusercontent.com/cryon-io/alis-cli/master/install.sh -O /tmp/install.sh && sh /tmp/install.sh;`
+
+# Usage
+
+`alis-cli` loads configuration by default from `/etc/alis-cli/alis.hjson` or `/etc/alis-cli/alis.json`. 
+You can specify custom configuration directory by passing `--config=<path to config file>`
+`alis-cli` passes all commands and options to `ami` of specified preconfigured apps.
+
+# Examples:
+
+1. Setup all apps
+   `alis-cli all setup`
+
+2. Setup app with id node1
+    `alis-cli node1 setup`
+
+3. Get info of all apps
+    `alis-cli all info`
+
+For all alis-cli specific options run `alis-cli --help`
+
+# Configuration
+
+Sample configuration:
+
+```hjson
+{
+    global: {
+        user: james
+    },
+    apps: [
+        {
+            id: node1,
+            type: nice.node,
+            configuration: {
+                ...
+            }
+        },
+        {
+            id: node2,
+            type: nice.node,
+            configuration: {
+                ...
+            },
+            user: george
+        },
+        ...
+    ]
+}
+```
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# install eli
+wget https://raw.githubusercontent.com/cryon-io/eli/master/install.sh -O /tmp/install.sh && sh /tmp/install.sh \
+&& ELI_DOWNLOADED="TRUE"
+
+if [ ! $ELI_DOWNLOADED = "TRUE" ]; then 
+    echo "Failed to download eli, please retry ... "
+    exit 1
+fi
+
+# install ami
+wget https://raw.githubusercontent.com/cryon-io/ami/master/install.sh -O /tmp/install.sh && sh /tmp/install.sh \
+&& AMI_DOWNLOADED="TRUE"
+
+if [ ! $AMI_DOWNLOADED = "TRUE" ]; then 
+    echo "Failed to download ami, please retry ... "
+    exit 1
+fi
+
+# install ami
+LATEST=$(curl -sL https://api.github.com/repos/cryon-io/alis-cli/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
+
+TMP_NAME="/tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+
+wget "https://github.com/cryon-io/ami/releases/download/$LATEST/alis-cli-unix-$(uname -p).zip" -O "$TMP_NAME" &&
+    printf "%s\n" "elify();zip.extract_file('$TMP_FILE', 'alis-cli-unix-$(uname -p)', '/usr/sbin/', { flattenRootDir = true})" | eli &&
+    mv "alis-cli-unix-$(uname -p)" "alis-cli"
+    chmod +x /usr/sbin/alis-cli &&
+    echo "alis-cli $LATEST successfuly installed."
